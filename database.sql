97f17c30e6c486f2f71560eddabd9d51b7093e19
Ярослав Родионов <iaroslav@MacBook-Pro--Iaroslav.local>
2025-05-10 16:06:34 +0300

    step 3

--- database.sql
+++ database.sql
@@ -71,4 +71,47 @@ CREATE TABLE teaching_groups (
     slug VARCHAR (20) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)
+);
+
+CREATE TYPE subscription AS ENUM ('active', 'pending', 'cancelled', 'completed');
+CREATE TABLE enrollments (
+    id BIGINT PRIMARY KEY,
+    user_id BIGINT NOT NULL REFERENCES users(id),
+    program_id BIGINT NOT NULL REFERENCES programs(id),
+    subscription_status subscription NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TYPE payment AS ENUM ('pending', 'paid', 'failed', 'refunded');
+CREATE TABLE payments (
+    id BIGINT PRIMARY KEY,
+    subscription_id BIGINT NOT NULL REFERENCES enrollments(id),
+    price NUMERIC (10, 2) NOT NULL,
+    payment_status payment NOT NULL,
+    payment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TYPE completion AS ENUM ('active', 'completed', 'pending', 'cancelled');
+CREATE TABLE program_completions (
+    id BIGINT PRIMARY KEY,
+    user_id BIGINT NOT NULL REFERENCES users(id),
+    program_id BIGINT NOT NULL REFERENCES programs(id),
+    completion_status completion NOT NULL,
+    start_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    finish_date TIMESTAMP,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE certificates (
+    id BIGINT PRIMARY KEY,
+    user_id BIGINT NOT NULL REFERENCES users(id),
+    program_id BIGINT NOT NULL REFERENCES programs(id),
+    certificates_url VARCHAR (200),
+    certificate_creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
