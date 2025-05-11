b807f4fdca455ecccb2ace78f4b3a8152693d134
Ярослав Родионов <iaroslav@MacBook-Pro--Iaroslav.local>
2025-05-11 12:29:11 +0300

    fix 7

--- database.sql
+++ database.sql
@@ -17,7 +17,7 @@ CREATE TABLE users(
         ON DELETE CASCADE,
     deleted_at TIMESTAMP DEFAULT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE programs(
@@ -145,7 +145,7 @@ CREATE TABLE discussions(
 );
 
 CREATE TYPE status AS ENUM ('created', 'in_moderation', 'published', 'archived');
-CREATE TABLE blog (
+CREATE TABLE blogs (
     id BIGINT PRIMARY KEY,
     user_id BIGINT NOT NULL REFERENCES users(id),
     title VARCHAR(50) NOT NULL,
